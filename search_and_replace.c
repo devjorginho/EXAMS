37599b9a7e21257d812def269ebe3b28e785ae4e
JorgeCarvalho <jcarvalho.swe@gmail.com>
2025-04-23 12:25:55 +0100

    search and replace solution

---
+++ search_and_replace.c
@@ -0,0 +1,41 @@
+//    ____                   __                 _       __        
+//   / __ \___ _   __       / /___  _________ _(_)___  / /_  ____ 
+//  / / / / _ \ | / /  __  / / __ \/ ___/ __ `/ / __ \/ __ \/ __ \
+// / /_/ /  __/ |/ /  / /_/ / /_/ / /  / /_/ / / / / / / / / /_/ /
+///_____/\___/|___/   \____/\____/_/   \__, /_/_/ /_/_/ /_/\____/ 
+//                                    /____/                     
+//	devjorginho 😎 | GitHub: https://github.com/devjorginho
+
+#include <unistd.h>
+
+size_t	avlen(char *s)
+{
+	size_t i;
+
+	i = 0;
+	while(s[i])
+		i++;
+	return (i);
+}
+char	*replaced(char *s, char c1, char c2)
+{
+	size_t	i;
+
+	i = 0;
+	while(s[i])
+	{
+		if (s[i] == c1)
+			s[i] = c2;
+		i++;
+	}
+	return(s);
+}
+int	main(int argc, char *argv[])
+{
+	size_t i;
+
+	i = 0;
+	if(argc == 4)
+		write(1, replaced(argv[1], argv[2][i], argv[3][i]), avlen(argv[1]));
+	write(1, "\n", 1);
+}

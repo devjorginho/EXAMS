f6e939dcf446588bf221d8eab302b06fef38fa84
JorgeCarvalho <jcarvalho.swe@gmail.com>
2025-06-02 13:13:29 +0100

    add max

--- EXAM_RANK_02/Level_02/max.c
+++ EXAM_RANK_02/Level_02/max.c
@@ -13,3 +13,27 @@ the array.
 The function returns the largest number found in the array.
 
 If the array is empty, the function returns 0.*/
+
+int		max(int* tab, unsigned int len)
+{
+	int i;
+	int big;
+
+	if(!tab || len == 0)
+		return(0);
+	i = 0;
+	big = tab[i];
+	while(i < len)
+	{
+		if(tab[i] > big)
+			big = tab[i];
+		i++;
+	}
+	return(big);
+}
+#include <stdio.h>
+int main()
+{
+	int nba[6] = {};
+	printf("%d", max(nba, 4));
+}

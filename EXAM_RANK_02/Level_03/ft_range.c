acbb8cb80f725a73e27106886b7e6b2d0870fbd7
devjorginho <jcarvalho.swe@gmail.com>
2025-06-04 00:26:32 +0100

    some ex of lvl3

--- EXAM_RANK_02/Level_03/ft_range.c
+++ EXAM_RANK_02/Level_03/ft_range.c
@@ -17,3 +17,42 @@ Examples:
 - With (-1, 2) you will return an array containing -1, 0, 1 and 2.
 - With (0, 0) you will return an array containing 0.
 - With (0, -3) you will return an array containing 0, -1, -2 and -3.*/
+
+#include <stdlib.h>
+
+int     *ft_range(int start, int end)
+{
+	int *range;
+	int i;
+	int len;
+
+	len = end - start;
+	if(len < 0 )
+		len = -len;
+	i = 0;
+	range = malloc((len + 1) * sizeof(int));
+	if(!range)
+		return(NULL);
+	if(end < start)
+	{
+		while(start >= end)
+			range[i++] = start--;
+	}
+	else
+	{
+		while(start <= end)
+		range[i++] = start++;
+	}
+	
+	return(range);
+}
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	int *nb = ft_range(0,4);
+	
+	while(i < 4)
+		printf("%d\n", nb[i++]);
+}
